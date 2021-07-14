929e31cb0488e049922f4f24c54122958c338252
David Konsumer <konsumer@jetboystudio.com>
2021-07-13 18:05:24 -0700

    got midi working

--- demos/midi.js
+++ demos/midi.js
@@ -1,4 +1,4 @@
-// this is a basic drum machine
+// this is a basic midi drum machine
 
 import core from 'elementary-core'
 import el from '@nick-thompson/elementary'
@@ -15,8 +15,8 @@ export default function () {
       gates[e.noteName] = e.type === 'noteOn' ? 1 : 0
     }
     const out = el.add(
-      el.mul(0.7, dt.kick(40, 0.104, 0.4, 0.4, 4, gates.C2)),
-      el.mul(0.6, dt.clap(800, 0.005, 0.204, gates.D2))
+      el.mul(0.7, dt.kick(40, 0.104, 0.4, 0.4, 4, el.const({ key: 'kick', value: gates.C2 }))),
+      el.mul(0.6, dt.clap(800, 0.005, 0.204, el.const({ key: 'clap', value: gates.D2 })))
     )
     core.render(out, out)
   })
