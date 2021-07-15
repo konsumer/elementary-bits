1ebe2832d2a77dc678e2c207a10c02db24792732
David Konsumer <konsumer@jetboystudio.com>
2021-07-14 17:47:30 -0700

    use require to cut down on variables

--- demos/abi.js
+++ demos/abi.js
@@ -1,5 +1,5 @@
 // this will just output node ABI version.
-import { getABIVersion, abiVersionMap } from 'node-abi-version'
+const { getABIVersion, abiVersionMap } = require('node-abi-version')
 const n = getABIVersion()
 console.log(Object.keys(abiVersionMap).find(v => abiVersionMap[v] === n))
 process.exit(0)
