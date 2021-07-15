7ddb2d316caee93b238b73a53bee53b99b115225
David Konsumer <konsumer@jetboystudio.com>
2021-07-14 17:44:56 -0700

    abi version for troubleshooting

--- demos/vmidi.js
+++ demos/vmidi.js
@@ -11,13 +11,13 @@ const gates = {
   D2: 0 // clap
 }
 
-const input = new midi.Input('Elementary Drum Machine', true)
-
-input.on('message', (deltaTime, message) => {
-  console.log(`m: ${message} d: ${deltaTime}`);
-})
-
 export default function () {
+  const input = new midi.Input('Elementary Drum Machine', true)
+
+  input.on('message', (deltaTime, message) => {
+    console.log(`m: ${message} d: ${deltaTime}`);
+  })
+  
   const out = el.add(
     el.mul(0.7, dt.kick(40, 0.104, 0.4, 0.4, 4, el.const({ key: 'kick', value: gates.C2 }))),
     el.mul(0.6, dt.clap(800, 0.005, 0.204, el.const({ key: 'clap', value: gates.D2 })))
